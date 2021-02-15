40e6f5d6a901e196f9d6ecab9f603cb82f20e8fc
Mgoumane <goumane.mahmoudou@gmail.com>
2021-02-15 16:27:15 +0100

    ok

---
+++ project/trippy/src/components/CityCard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class CityCard extends Component {
+    render() {
+        return (
+            <>
+            <div className="card" style="width: 18rem;">
+                <img src={this.props.image} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <p className="card-text">{this.props.cities}</p>
+                    </div>
+            </div>
+            </>
+        )
+    }
+}
--- project/trippy/src/views/Home.jsx
+++ project/trippy/src/views/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import getHomeData from "../utils/Api.js"
+import CityCard from "../components/CityCard"
 
 export default class Home extends Component {
     constructor(props) {
@@ -18,8 +19,11 @@ export default class Home extends Component {
             <div>
                 <h1>Découvrir le monde</h1>
                 {this.state.cities.length > 0 &&
-                    <p>il y a quelque chose dans l'array</p>
-                }
+                   this.state.cities.map ((cities) => {
+                       return( 
+                           <CityCard cities={this.state} ></CityCard>
+                       )
+                    })                 }
                 {this.state.cities.length === 0 &&
                     <p>Il n'y a rien dans l'array</p>
                 }
