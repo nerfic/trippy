48163f8ae4a0cf8f7dcfd61e79f6ba09190a9770
Allan <jdll.allan@gmail.com>
2021-02-17 11:25:22 +0100

    add map / hotelmarkers

--- project/trippy/src/views/Hotels.jsx
+++ project/trippy/src/views/Hotels.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import HotelCard from "../components/HotelCard"
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import HotelMap from "../components/HotelMap"
 
 export default class Hotels extends Component {
     constructor(props) {
         super(props)
         this.state = {
             city: "",
-            coords: ""
+            coords: {
+                lat: 0,
+                lon: 0
+            }
         }
     }
 
@@ -20,38 +24,40 @@ export default class Hotels extends Component {
                     coords: response.center
                 })
             })
+
+        // console.log("coords hotel", this.state.city.location.lat)
     }
 
     render() {
+        console.log(this.state.coords)
+        let newLat = this.state.coords.lat
+        let newLon = this.state.coords.lon
+        console.log("lat =", newLat)
+        console.log("lon =", newLon)
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-6">
                         {this.state.city.length > 0 &&
-                            this.state.city.map((hotel, index) => {
-                                return (
-                                    <HotelCard
-                                        link={hotel._id}
-                                        name={hotel.name}
-                                        image={"http://localhost:3000" + hotel.pictures[0]}
-                                        price={hotel.price}
-                                        star={hotel.stars}
-                                    />
-                                )
-                            })
+                            <div>
+                                {this.state.city.map((hotel, index) => {
+                                    return (
+                                        <HotelCard
+                                            link={hotel._id}
+                                            name={hotel.name}
+                                            image={"http://localhost:3000" + hotel.pictures[0]}
+                                            price={hotel.price}
+                                            star={hotel.stars}
+                                        />
+                                    )
+                                })}
+                                <HotelMap
+                                    cityLatitude={newLat}
+                                    cityLongitude={newLon}
+                                    hotels={this.state.city}
+                                />
+                            </div>
                         }
-                        <Map center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "500px" }}>
-                            <TileLayer
-                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            />
-                            <Marker position={[51.505, -0.09]}>
-                                <Popup>
-                                    A pretty CSS3 popup. <br /> Easily customizable.
-                                </Popup>
-                            </Marker>
-                        </Map>
-                        <p>test</p>
                     </div>
                 </div>
                 {this.state.city.length === 0 &&
