fb30e30d687b3254d61a3d268a150a27268bb7c1
Allan <jdll.allan@gmail.com>
2021-02-17 11:54:48 +0100

    get hotel/id api

--- project/trippy/src/views/HotelsPage.jsx
+++ project/trippy/src/views/HotelsPage.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
 
 export default class HotelsPage extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            hotel: ""
+        }
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:3002/api/hotels/${this.props.match.params.id}`)
+            .then(response => response.json())
+            .then(response => {
+                this.setState({
+                    hotel: response
+                })
+            })
+    }
+
     render() {
         return (
             <div>
